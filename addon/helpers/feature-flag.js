95db7a64b1495703b8d4a6a586709f076047f5c9
LABS-WANGZ-MAC <wangz@garena.com>
2016-01-12 14:30:31 +0800

    Add HTMLBars, JavaScript defeatureify

--- addon/helpers/feature-flag.js
+++ addon/helpers/feature-flag.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import getOwner from 'ember-getowner-polyfill';
 
 export function featureFlag() {
-
+  Ember.assert('Helper `featureFlag` cannot run at runtime and should be removed at compile-time. ', false);
 }
 
 export default Ember.Helper.helper(featureFlag);
--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-/* jshint node: true */
+/*jshint node:true*/
+/* global require, module */
 'use strict';
-var BabelFeatureFlag = require('./babel-defeatureify');
-function defeatureifyTemplate(element) {
-  if (element.type === 'BlockStatement') {
-    if (element.path.original === 'if') {
-      if (element.params.length === 1) {
-        var subexp = element.params[0];
-        if (subexp.type === 'SubExpression' && subexp.path.original === 'feature-flag') {
-          var feature = subexp.params[0].value;
-          console.log(feature);
-        }
-      }
-    }
-  }
-  if (element.children) {
-    element.children.forEach(defeatureifyTemplate);
-  }
-}
+var BabelDefeatureify = require('./lib/babel-defeatureify');
+var TemplateDefeatureify = require('./lib/template-defeatureify');
+var StyleDefeatureify = require('./lib/style-defeatureify');
+
 module.exports = {
   name: 'ember-feature-flag-solution',
-  included: function () {
-    this.app.babel = {
-      plugins: [
-        BabelFeatureFlag({
-          import: {
-            module: 'ember-feature-flag-solution/helpers/feature-flag',
-            name: 'featureFlag'
-          },
-          features: this.featureFlags()
-        })
-      ]
-    };
-    this.app.registry.add('htmlbars-ast-plugin', {
-      name: 'feature-flag-template-defeatureify',
-      plugin: function () {
-        return {
-          transform: function (node) {
-            node.body.forEach(defeatureifyTemplate);
-            return node;
-          }
-        };
-      }
+  included: function (app) {
+
+    app.options.featureFlag = app.options.featureFlag || {};
+    app.options.featureFlag.features = app.options.featureFlag.features || {};
+    app.options.featureFlag.development = app.options.featureFlag.development || false;
+    app.options.featureFlag.production = app.options.featureFlag.production || true;
+
+    // Add babel plugin to filter out `featureFlag` function in JS files
+    var babelDefeatureifyInstance = BabelDefeatureify({
+      import: {
+        module: 'ember-feature-flag-solution/helpers/feature-flag',
+        name: 'featureFlag'
+      },
+      features: app.options.featureFlag
     });
-  },
-  featureFlags: function () {
-    return this.app.featureFlags || {};
+
+    if (app.options.babel.plugins) {
+      app.options.babel.plugins.push(babelDefeatureifyInstance);
+    } else {
+      app.options.babel.plugins = [babelDefeatureifyInstance];
+    }
+
+    // Add htmlbars-ast-plugin to filter out `feature-flag` helpers in templates
+    app.registry.add('htmlbars-ast-plugin', {
+      name: 'feature-flag-template-defeatureify',
+      plugin: TemplateDefeatureify({
+        features: app.options.featureFlag
+      })
+    });
+
+    // Add css preprocessor to output feature flag constants
+    // app.registry.add('css', )
   },
   isDevelopingAddon: function () {
     return true;
