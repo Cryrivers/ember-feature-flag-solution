a729a587129d03b85d11920cb66edffe7c9636bf
LABS-WANGZ-MAC <wangz@garena.com>
2016-01-12 15:02:15 +0800

    More verbose assert message

--- addon/helpers/feature-flag.js
+++ addon/helpers/feature-flag.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 import getOwner from 'ember-getowner-polyfill';
 
-export function featureFlag() {
-  Ember.assert('Helper `featureFlag` cannot run at runtime and should be removed at compile-time. ', false);
+export function featureFlag([featureName]) {
+  Ember.assert(`Helper featureFlag('${featureName}') cannot run at runtime and should be removed at compile-time. `, false);
 }
 
 export default Ember.Helper.helper(featureFlag);
