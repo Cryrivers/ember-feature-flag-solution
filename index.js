fd76d3043b6dc0efc5bfb79b643e62b1d5442795
Wangz <LABS-WANGZ-MAC@labs-hany-pc.sg.garena.com>
2016-01-14 21:52:41 +0800

    Be able to skip code stripping on certain environments by adding option `strip` (true by default)

--- index.js
+++ index.js
@@ -13,62 +13,71 @@ module.exports = {
     this._super.included.apply(this, arguments);
     this.setupPreprocessorRegistry('parent', app.registry);
 
-    // Add babel plugin to filter out `featureFlag` function in JS files
-    var babelDefeatureifyInstance = BabelDefeatureify({
-      import: {
-        module: 'ember-feature-flag-solution/helpers/feature-flag',
-        name: 'featureFlag'
-      },
-      features: this.featureFlag.features,
-      verbose: this.featureFlag.verbose
-    });
-
-    if (app.options.babel.plugins) {
-      app.options.babel.plugins.push(babelDefeatureifyInstance);
-    } else {
-      app.options.babel.plugins = [babelDefeatureifyInstance];
-    }
-
-    // Add htmlbars-ast-plugin to filter out `feature-flag` helpers in templates
-    app.registry.add('htmlbars-ast-plugin', {
-      name: 'feature-flag-template-defeatureify',
-      plugin: TemplateDefeatureify({
+    if (this.featureFlag.strip) {
+      // Add babel plugin to filter out `featureFlag` function in JS files
+      var babelDefeatureifyInstance = BabelDefeatureify({
+        import: {
+          module: 'ember-feature-flag-solution/helpers/feature-flag',
+          name: 'featureFlag'
+        },
         features: this.featureFlag.features,
         verbose: this.featureFlag.verbose
-      })
-    });
+      });
+
+      if (app.options.babel.plugins) {
+        app.options.babel.plugins.push(babelDefeatureifyInstance);
+      } else {
+        app.options.babel.plugins = [babelDefeatureifyInstance];
+      }
+
+      // Add htmlbars-ast-plugin to filter out `feature-flag` helpers in templates
+      app.registry.add('htmlbars-ast-plugin', {
+        name: 'feature-flag-template-defeatureify',
+        plugin: TemplateDefeatureify({
+          features: this.featureFlag.features,
+          verbose: this.featureFlag.verbose
+        })
+      });
+    }
+
   },
   setupPreprocessorRegistry(type, registry) {
     if (registry.app.options) {
+
       var appEnvironmentOptions = registry.app.project.config(registry.app.env);
       this.featureFlag = appEnvironmentOptions.featureFlag || {};
       this.featureFlag.features = this.featureFlag.features || {};
       this.featureFlag.includeFileByFlag = this.featureFlag.includeFileByFlag || {};
       this.featureFlag.verbose = this.featureFlag.verbose || true;
-      this.featureFlag.development = this.featureFlag.development || false;
-      this.featureFlag.production = this.featureFlag.production || true;
-      // Add css plugin to provide feature flags to SCSS
-      registry.add('css', new StyleDefeatureify({
-        features: this.featureFlag.features
-      }));
+      this.featureFlag.strip = this.featureFlag.strip || true;
+      if (this.featureFlag.strip) {
+        // Add css plugin to provide feature flags to SCSS
+        registry.add('css', new StyleDefeatureify({
+          features: this.featureFlag.features
+        }));
+      }
     }
   },
   postprocessTree: function(type, tree) {
     if (type !== 'js') {
       return tree;
     } else {
-      var features = this.featureFlag.features;
-      var includeFileByFlag = this.featureFlag.includeFileByFlag;
-      var excludes = [];
-      Object.keys(features).forEach(function(flag) {
-        if (!features[flag] && includeFileByFlag[flag]) {
-          excludes = excludes.concat(includeFileByFlag[flag]);
-        }
-      });
-      return new Funnel(tree, {
-        exclude: excludes,
-        description: 'Funnel: Conditionally Filtered Files by Feature Flags'
-      });
+      if (this.featureFlag.strip) {
+        var features = this.featureFlag.features;
+        var includeFileByFlag = this.featureFlag.includeFileByFlag;
+        var excludes = [];
+        Object.keys(features).forEach(function(flag) {
+          if (!features[flag] && includeFileByFlag[flag]) {
+            excludes = excludes.concat(includeFileByFlag[flag]);
+          }
+        });
+        return new Funnel(tree, {
+          exclude: excludes,
+          description: 'Funnel: Conditionally Filtered Files by Feature Flags'
+        });
+      } else {
+        return tree;
+      }
     }
   },
   isDevelopingAddon: function() {
